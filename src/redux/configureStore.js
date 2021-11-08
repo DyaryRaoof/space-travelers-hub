cb0bf3eaa155eab75f823703f05677110c4d5304
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-11-08 22:53:42 +0300

    Install redux-thunk and axios

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,9 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import getMissionsReducer from './missions/missions';
 
 const reducers = combineReducers({ missions: getMissionsReducer });
-const store = createStore(reducers, applyMiddleware(logger));
+const store = createStore(reducers, applyMiddleware(thunk, logger));
 
 export default store;
