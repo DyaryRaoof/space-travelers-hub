e8ce13f01aa5e454675eb55d792c45cfbb082918
nachosala <nachosala89@gmail.com>
2021-11-09 11:04:00 -0300

    Display rockets list

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -4,16 +4,30 @@ import { fetchRockets } from '../redux/rockets/rockets';
 
 const Rockets = () => {
   const dispatch = useDispatch();
-  const rockets = useSelector((state) => state.rocketsReducer);
+  const rockets = useSelector((state) => state.rocketsReducer.rockets);
 
   useEffect(() => {
-    if (Object.keys(rockets).length === 0) {
+    if (rockets === undefined) {
       dispatch(fetchRockets());
     }
   }, []);
 
   return (
-    <div>Hello From Rockets</div>
+    <ul className="container">
+      {(typeof rockets !== 'undefined') ? rockets.map((rocket) => (
+        <li key={rocket.id} className="row">
+          <div className="col-md-3">
+            <img src={rocket.flickr_images[0]} alt="Rocket" />
+          </div>
+          <div className="col-md-9 d-flex flex-column">
+            <h2 className="rocket-name">{rocket.rocket_name}</h2>
+            <p className="rocket-description">{rocket.description}</p>
+            <button type="button">Reserve Rocket</button>
+          </div>
+        </li>
+      ))
+        : <li>Loading...</li>}
+    </ul>
   );
 };
 
