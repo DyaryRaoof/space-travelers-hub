0e8e4931934440691b4181fee5f5229519b2de1e
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-11-09 19:35:59 +0300

    Add joined missions to display in MyProfile Component

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,30 @@
-const MyProfile = () => (
-  <div>Hello From MyProfile</div>
-);
+import {
+  Container, Col, Row, ListGroup,
+} from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import './MyProfile.css';
+
+const MyProfile = () => {
+  const reserverdMissions = useSelector((state) => state.missionsReducer
+    .missions.filter((mission) => mission.reserved === true));
+
+  return (
+    <Container className="profile-container">
+      <Row>
+        <Col>
+          <h1>My Missions</h1>
+          <ListGroup>
+            {reserverdMissions.map((mission) => (
+              <ListGroup.Item key={mission.mission_id}>
+                {mission.mission_name}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+        <Col>2 of 2</Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default MyProfile;
