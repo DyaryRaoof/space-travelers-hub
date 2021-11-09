ffd0fd3485b08a6b05b01a79859ddf4798d8adab
nachosala <nachosala89@gmail.com>
2021-11-09 12:19:14 -0300

    Add styling to rockets list

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchRockets } from '../redux/rockets/rockets';
+import './Rockets.css';
 
 const Rockets = () => {
   const dispatch = useDispatch();
@@ -15,11 +16,9 @@ const Rockets = () => {
   return (
     <ul className="container">
       {(typeof rockets !== 'undefined') ? rockets.map((rocket) => (
-        <li key={rocket.id} className="row">
-          <div className="col-md-3">
-            <img src={rocket.flickr_images[0]} alt="Rocket" />
-          </div>
-          <div className="col-md-9 d-flex flex-column">
+        <li key={rocket.id} className="row mb-3">
+          <img src={rocket.flickr_images[0]} alt="Rocket" className="col-lg-3 col-md-6 mb-3 mb-lg-0" />
+          <div className="col-lg-9 d-flex flex-column align-items-start">
             <h2 className="rocket-name">{rocket.rocket_name}</h2>
             <p className="rocket-description">{rocket.description}</p>
             <button type="button">Reserve Rocket</button>
