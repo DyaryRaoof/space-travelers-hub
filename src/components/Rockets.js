c4a54da9dff6e5c3b384c96604caa57156010664
nachosala <nachosala89@gmail.com>
2021-11-09 08:55:22 -0300

    Fetch only once

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,12 +1,15 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchRockets } from '../redux/rockets/rockets';
 
 const Rockets = () => {
   const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rocketsReducer);
 
   useEffect(() => {
-    dispatch(fetchRockets());
+    if (Object.keys(rockets).length === 0) {
+      dispatch(fetchRockets());
+    }
   }, []);
 
   return (
